2c58e8c2232ab76c41c7fdb096eaf070d87d97d3
Tsionalemu <tsionalemuta@gmail.com>
2025-09-28 00:35:49 +0300

    Add log_queries decorator and fetch_all_users example

--- 0-log_queries.py
+++ 0-log_queries.py
@@ -1,3 +1,21 @@
+import sqlite3
+import functools
+
+#### decorator to lof SQL queries
+
+ """ YOUR CODE GOES HERE"""
+
+@log_queries
+def fetch_all_users(query):
+    conn = sqlite3.connect('users.db')
+    cursor = conn.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+#### fetch users while logging the query
+users = fetch_all_users(query="SELECT * FROM users")
 #!/usr/bin/env python3
 import sqlite3
 import functools
